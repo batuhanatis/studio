ebea911e7c960ea0accaca5cdae682da5dd1af10
Batuhan Atis <batuhan.atis@ozu.edu.tr>
2025-07-01 20:50:12 +0000

    Runtime Error
    FirebaseError: Failed to get document because the client

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
 
 // IMPORTANT: Replace the following with your app's Firebase project configuration.
 // See: https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,5 +18,18 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a time.
+      console.warn("Firestore persistence failed: Multiple tabs open.");
+    } else if (err.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      console.warn("Firestore persistence not supported in this browser.");
+    }
+  });
 
 export { app, auth, db };
