76afd56e7c861375c27f21b1c41da8d6292da0f7
Batuhan Atis <batuhan.atis@ozu.edu.tr>
2025-07-12 13:02:41 +0000

    Attempt 1 failed: Permission denied. Retrying in 200ms...
    Attempt 2 fa

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,11 +4,11 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAyPvD9_qqD8R3ljCAjF5oCAOJJnU320Xk",
+  apiKey: "AIzaSyAyPvD9_qqD9_qqD8R3ljCAjF5oCAOJJnU320Xk",
   authDomain: "movie-finder-kqqse.firebaseapp.com",
   databaseURL: "https://movie-finder-kqqse-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "movie-finder-kqqse",
-  storageBucket: "movie-finder-kqqse.firebasestorage.app",
+  storageBucket: "movie-finder-kqqse.appspot.com",
   messagingSenderId: "226527022824",
   appId: "1:226527022824:web:78c6557a8005dec26e435d"
 };
